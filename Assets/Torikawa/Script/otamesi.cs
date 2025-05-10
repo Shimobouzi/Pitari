09f2a9f73476f75d44dcd3829bde2d66f3dcb2da
OsAwakirot <toritorisousou@gmail.com>
2025-05-10 23:53:58 +0900

    5月10日　コントローラ繋げれているか確認したためお試しスクリプトを入れた

---
+++ Assets/Torikawa/Script/otamesi.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class otamesi : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnMove()
+    {
+        this.transform.position = new Vector3(3, 0, 0);
+    }
+}
