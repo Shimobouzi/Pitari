064c831d20eb64324b99e8b713657d96b94b0eff
S_H\shimo <shimobou8810@icloud.com>
2025-05-17 18:51:34 +0900

    敵途中

--- Assets/Main/Script/Enemy/EnemyBase.cs
+++ Assets/Main/Script/Enemy/EnemyBase.cs
@@ -1,29 +1,109 @@
+using System.Collections;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class EnemyBase : MonoBehaviour
 {
-    public int speed;
-    public int coolTime;
-    void Start()
+    public static EnemyBase instance;
+
+    protected float nomalSpeed = 2f;
+    protected float runSpeed = 10f;
+    protected float coolTime;
+    protected int isLeft = 1;
+    protected Vector3 thisScale;
+
+    protected void Awake()
     {
-        
+        instance = this;
+    }
+    protected void Start()
+    {
+        thisScale = transform.localScale;
+        Idel();
     }
 
     // Update is called once per frame
-    void Update()
+    protected void Update()
     {
-        
+        transform.localScale = new Vector3(thisScale.x * isLeft, thisScale.y, thisScale.z);
     }
 
-    public void Idel()
+    protected void OnTriggerStay(Collider other)
     {
-
+        if (other.gameObject.tag == "Player")
+        {
+            StopAllCoroutines();
+            StartCoroutine(ChasePlayer(5f, runSpeed));
+        }
     }
 
-    public void Encount()
+    protected void OnCollisionEnter(Collision collision)
     {
-
+        if (collision.gameObject.tag == "Player")
+        {
+            StopAllCoroutines();
+            StartCoroutine(AttackPlayer());
+        }
     }
 
-    //public void 
+    protected void Idel()
+    {
+        StartCoroutine(LeftRightMove(5f, nomalSpeed));
+    }
+
+    protected IEnumerator ChasePlayer(float duration, float moveSpeed)
+    {
+        if (isLeft == 1)
+        {
+            StartCoroutine(MoveLeft(duration, moveSpeed));
+        }
+        else
+        {
+            StartCoroutine(MoveRight(duration, moveSpeed));
+        }
+        yield return new WaitForSeconds(duration);
+        Idel();
+    }
+
+    protected IEnumerator AttackPlayer()
+    {
+        JoyconController.Instance.OnBuruBuru();
+        yield return new WaitForSeconds(1f);
+        PitariSceneManager.Instance.ToResult(false);
+    }
+
+    protected IEnumerator MoveRight(float duration, float moveSpeed)
+    {
+        isLeft = -1;
+        float timer = 0f;
+        while (timer < duration)
+        {
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime); // 2Dなら右方向に移動
+            //一秒間に指定した数値分右に動く処理
+            timer += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    protected IEnumerator MoveLeft(float duration, float moveSpeed)
+    {
+        isLeft = 1;
+        float timer = 0f;
+        while (timer < duration)
+        {
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime); // 2Dなら左方向に移動
+            //一秒間に指定した数値分右に動く処理
+            timer += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    protected IEnumerator LeftRightMove(float duration, float moveSpeed)
+    {
+        StartCoroutine(MoveRight(duration, moveSpeed));
+        yield return new WaitForSeconds(duration);
+        StartCoroutine(MoveLeft(duration, moveSpeed));
+        yield return new WaitForSeconds(duration);
+        StartCoroutine(LeftRightMove(duration, moveSpeed));
+    }
 }
--- Assets/Main/Script/System/PitariSceneManager.cs
+++ Assets/Main/Script/System/PitariSceneManager.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 public class PitariSceneManager : MonoBehaviour
 {
     public static PitariSceneManager Instance;
+    public static bool isClear;
 
     private void Awake()
     {
@@ -16,8 +17,9 @@ public class PitariSceneManager : MonoBehaviour
         SceneManager.LoadScene("Title");
     }
 
-    public void ToResult()
+    public void ToResult(bool isClear_)
     {
+        isClear = isClear_;
         SceneManager.LoadScene("Result");
     }
 
