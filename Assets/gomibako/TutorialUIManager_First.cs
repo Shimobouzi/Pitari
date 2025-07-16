1738c9eb280f545948036e8cd16b31e024339994
SO-GALLERIA\torit <toritorisousou@gmail.com>
2025-07-17 04:50:38 +0900

    dekitekitakana

---
+++ Assets/gomibako/TutorialUIManager_First.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TutorialUIManager_First : MonoBehaviour
+{
+    public static event System.Action OnTutorialClosed;
+
+    [Header("UI要素")]
+    public GameObject pausePanel;
+    public GameObject speechBubble;
+    public Image speechImage;
+
+    [Header("狸関連")]
+    public Transform tanuki;
+    public Vector3 tanukiTargetPosition;
+    public float tanukiMoveSpeed = 2f;
+    private bool isTanukiMoving = false;
+
+    [Header("画像設定")]
+    public List<Sprite> tutorialSprites = new List<Sprite>();
+    public float imageDisplayTime = 2.5f;
+
+    private bool isTutorialRunning = false;
+
+    void Start()
+    {
+        pausePanel.SetActive(false);
+        speechBubble.SetActive(false);
+        tanuki.gameObject.SetActive(false);
+    }
+
+    public void ShowTutorial()
+    {
+        if (isTutorialRunning) return;
+
+        pausePanel.SetActive(true);
+        tanuki.gameObject.SetActive(true);
+
+        SpriteRenderer sr = tanuki.GetComponent<SpriteRenderer>();
+        if (sr != null) sr.sortingOrder = 10;
+
+        isTanukiMoving = true;
+        isTutorialRunning = true;
+    }
+
+    void Update()
+    {
+        if (isTanukiMoving)
+        {
+            tanuki.position = Vector3.MoveTowards(tanuki.position, tanukiTargetPosition, tanukiMoveSpeed * Time.deltaTime);
+
+            if (Vector3.Distance(tanuki.position, tanukiTargetPosition) < 0.1f)
+            {
+                isTanukiMoving = false;
+                StartCoroutine(ShowImageSequence());
+            }
+        }
+    }
+
+    IEnumerator ShowImageSequence()
+    {
+        speechBubble.SetActive(true);
+
+        foreach (Sprite sprite in tutorialSprites)
+        {
+            speechImage.sprite = sprite;
+            yield return new WaitForSeconds(imageDisplayTime);
+        }
+
+        speechBubble.SetActive(false);
+        pausePanel.SetActive(false);
+        tanuki.gameObject.SetActive(false);
+        isTutorialRunning = false;
+
+        OnTutorialClosed?.Invoke();
+    }
+}
